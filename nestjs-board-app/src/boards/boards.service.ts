231c5f1866cf6e94935b6eb8636b4959e5e5a297
ljh9 <dkssud2208@naver.com>
2023-03-11 18:01:11 +0900

    Update repository

--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -32,15 +32,9 @@ export class BoardsService {
     //     return board;
     // }
 
-    async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-        const {title, description} = createBoardDto;
-        const board = this.boardRepository.create({
-            title, 
-            description, 
-            status: BoardStatus.PUBLIC
-        })
-        await this.boardRepository.save(board);
-        return board;
+    createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        
+        return this.boardRepository.createBoard(createBoardDto);
     }
 
     async getBoardById(id): Promise <Board> {
