8e05b04cd219c0b3c1d7a7f6f5a9a6d670337036
ljh9 <dkssud2208@naver.com>
2023-03-19 20:19:28 +0900

    Add Create

--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,7 +1,12 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    
+    async createUser(authCredentialsdto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsdto;
+        const user = this.create({ username, password });
+        await this.save(user);
+    }
 }
