a695968cf16ce0d04987848904c409f281e38a24
ljh9 <dkssud2208@naver.com>
2023-03-22 19:57:58 +0900

    Add unique

--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
@@ -7,6 +8,16 @@ export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password });
-        await this.save(user);
+
+        try {
+            await this.save(user);
+        } catch (error) {
+            if(error.code ==='23505'){
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+        
     }
 }
