f84d67011e198094e3b51d4844d8cba0daf97afe
ljh9 <dkssud2208@naver.com>
2023-03-27 02:24:22 +0900

    Add Jwt

--- nestjs-board-app/src/auth/auth.service.ts
+++ nestjs-board-app/src/auth/auth.service.ts
@@ -3,24 +3,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt/dist';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository 
+        private userRepository: UserRepository, 
+        private jwtService: JwtService
     ){}
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.createUser(authCredentialsDto);
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         const { username, password } = authCredentialsDto;
         const user = await this.userRepository.findOneBy({ username });
 
         if(user && (await bcrypt.campare(password, user.password))) {
-            return 'success';
+            const payload = { username };
+            const accessToken = await this.jwtService.sign(payload);
+            return { accessToken };
         } else {
             throw new UnauthorizedException('fail')
         }
