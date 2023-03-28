4f47aa108a3fde9607ae17930fb6dfb4e3895aaf
ljh9 <dkssud2208@naver.com>
2023-03-28 21:23:17 +0900

    Add Passport, Jwt

---
+++ nestjs-board-app/src/auth/jwt.strategy.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Strategy } from "passport-jwt";
+import { UserRepository } from "./user.repository";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository
+    ){
+        super({
+            
+        })
+    }
+}
